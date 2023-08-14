17c8904a5bee84c5dd54895c50b1746ddc8ffea9
Ote Ian <oteian9@gmail.com>
2023-08-14 06:05:51 -0700

    Lists of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print(None)
+    if matrix is None or not matrix:
+        print("")
+        return
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=' ')
+        if not matrix[i]:
+            print("")
+        else:
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=' ')
