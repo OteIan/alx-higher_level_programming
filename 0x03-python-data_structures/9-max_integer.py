9a6cbf258fc5c288cd49162dfa262acd379a5914
Ote Ian <oteian9@gmail.com>
2023-08-14 07:22:24 -0700

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    num_max = 0
+    for num in my_list :
+        if num > num_max:
+            num_max = num
+    return num_max
