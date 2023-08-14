25c65a1fea6096b10ba6bd770bd801efa2f48f31
Ote Ian <oteian9@gmail.com>
2023-08-14 07:04:01 -0700

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    # Extract the first 2 integers from each tuple
+    # Or use 0 if not available
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
 
+    # Calculate the sum of the corresponding elements in each tuple
     sum_first = a[0] + b[0]
     sum_second = a[1] + b[1]
 
