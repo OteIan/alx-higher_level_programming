9eba437efb85ae0273fbb24cbd64bbd1271e0ab9
Ote Ian <oteian9@gmail.com>
2023-08-14 21:14:04 +0300

    Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,16 +10,61 @@
 int is_palindrome(listint_t **head)
 {
     listint_t *current = *head;
+    listint_t *turtle, *hare;
     int count = 0;
-
+  
     if (head == NULL || *head == NULL)
         return (1);
 
+    turtle = hare = *head;
     while (current != NULL)
     {
-        count++;
         current = current->next;
+        count++;
+    }
+    while (hare != NULL)
+    {
+        turtle = turtle->next;
+        hare = hare->next->next;
+    }
+    
+    turtle = reverse_list(turtle);
+    current = *head;
+    while (current != NULL && turtle != NULL)
+    {
+        if (current->n == turtle->n)
+        {
+            current = current->next;
+            turtle = turtle->next;
+            continue;
+        }
+        else
+            break;
+    }
+    /* Return 1 if both current and turtle are NULL, else return 0 */
+    return (current == NULL && turtle == NULL);
+}
+
+/**
+ * reverse_list - Reverses a singly linked list
+ * 
+ * @head: Pointer to the head node of the list
+ * 
+ * Return: Pointer to the head node of the reversed list
+*/
+listint_t *reverse_list(listint_t *head)
+{
+    listint_t *prev = NULL, *next = NULL;
+    listint_t *current = head;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+
+        prev = current;
+        current = next;
     }
 
-    
+    return (prev);
 }
