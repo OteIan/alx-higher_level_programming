02015385c8dc621b376613f2b3fa24c9b163b7fc
Ote Ian <oteian9@gmail.com>
2023-08-16 11:25:15 -0700

    Weughted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    total_weight = 0
+    total_score = 0
+    for score, weight in my_list:
+        total_weight += (score * weight)
+        total_score += weight
+        # I'm sorry if you don't understand this
+        # It works so i'm not touching it
+    return total_weight / total_score
