5f9a83e0aa8f5df76e229368bbc21322a57f5f1a
Ote Ian <oteian9@gmail.com>
2023-08-16 09:52:27 -0700

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,14 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
     num = 0
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
     for ch in range(len(roman_string) - 1):
         if roman[roman_string[ch]] < roman[roman_string[ch + 1]]:
-            roman[roman_string[ch]] = -(roman[roman_string[ch]])
-        num += roman[roman_string[ch]]
-
-    num += roman[roman_string[-1]]
+            num -= roman[roman_string[ch]]
+        else:
+            num += roman[roman_string[ch]]
+    num += roman[roman_string[-1]] # Adds the last character
     return num
