a770633db24ccc876a5df8f96b25a57df452e150
OteIan <oteian9@gmail.com>
2023-08-30 11:56:34 -0700

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,23 +3,17 @@
 
 
 class Node:
-    """
-    This defines a node based one the provided data and next node object
-
-    Attributes:
-        __data (int): Data to be help by the node (Private attribute)
-        __next_node (Node): Pointer to the next node (Private attribute)
-    """
+    """Represent a node in a singly linked list"""
     def __init__(self, data, next_node=None):
         """
         This initilazes a new node instance
 
         Args:
             data (int): Data stored by the node
-            next_node (Node): Pointer to the next node (Default is None)
+            next_node (Node): The next node of the new node
         """
-        self.data = data
-        self.next_node = next_node
+        self._data = data
+        self._next_node = next_node
 
     @property
     def data(self):
@@ -29,8 +23,8 @@ class Node:
         Return:
             int: Data stored by the node
         """
-        return self.data
-    
+        return self.__data
+
     @data.setter
     def data(self, value):
         """
@@ -38,14 +32,14 @@ class Node:
 
         Args:
             value (int): Data stored by the node
-        
+
         Raises:
             TypeError: If value is not an integer
         """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
-        
+
     @property
     def next_node(self):
         """
@@ -54,8 +48,8 @@ class Node:
         Return:
             Node: Pointer to the next node
         """
-        return self.next_node
-    
+        return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
         """
@@ -63,41 +57,50 @@ class Node:
 
         Args:
             value (Node): Pointer to the next node
-        
+
             Raises:
                 TypeError: If value is not a node object
         """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a node abject")
-        self.__next.node = value
+        self.__next_node = value
+
 
 class SinglyLinkedList:
-    """
-    This defines a SinglyLinkedList based on the provided nodes
-
-    Attribute:
-        __head (Node): Pointer to the head node of the list
-    """
+    """This represents a singly linked list"""
     def __init__(self):
-        self.head = None
+        """This initializes a new singly linked list"""
+        self.__head = None
 
     def sorted_insert(self, value):
         """
-        This defines a new sorted_insert instance
+        Inserts a new node to a singly linked list
+
+        It is inserted at the correct numerical position based on the data
 
         Args:
-            value (int): Value to be stored by the node
+            value (Node): The new node to insert
         """
         new_node = Node(value)
 
-        if self.head is None or value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
-            return
-        
-        current = self.head
-        while current.next_node is not None or current.next_node.data < value:
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new
+        else:
+            current = self__.head
+            while current.next_node is not None or current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """This defines print() representation of a SinglyLinkedList"""
+        nodes = []
+        current = self.__head
+        while current is not None:
+            nodes.append(str(current.data))
             current = current.next_node
-        
-        new_node.next_node = current.next_node
-        current.next_node = new_node
+        return '\n'.join(nodes)
