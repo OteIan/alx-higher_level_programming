af2e52b96f05f8969b47c7b68bbb4019092ff5a5
OteIan <oteian9@gmail.com>
2023-09-05 04:49:53 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,16 +5,16 @@
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given divisor
- 
+
     Args:
         matrix (List[List[Union[int,float]]]): 2 dimensional list containing
             integers or floats
         div (int): Divisor
-    
+
     Return:
         list[list[float]]: A new matrix where all elements are divided by 'div,
             rounded to 2 decimal places
-    
+
     Raises:
         TypeError: If 'matrix' is not a valid matrix of integers/floats,
             or if 'div' is not a number, or if all rows in 'matrix' are not
@@ -23,14 +23,14 @@ def matrix_divided(matrix, div):
     """
     if matrix is None or div is None:
         return None
-    
+
     # Checks if 'matrix' is a list of lists containing integers/floats
     if not (isinstance(matrix, list) and
-        all(isinstance(row, list) for row in matrix) and
-        all(isinstance(value, (int, float))for row in matrix
+            all(isinstance(row, list) for row in matrix) and
+            all(isinstance(value, (int, float))for row in matrix
             for value in row)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+
     # Check if every row of matrix is of the same length
     first_row_length = len(matrix[0])
     # Starts to iterate through the matrix from the second row
@@ -45,5 +45,6 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     elif type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-    
-    return list(map(lambda y: list(map(lambda x: round(x/div, 2), y)), new_matrix))
+
+    return list(map(lambda y: list(map(lambda x: round(x/div, 2), y)),
+                    new_matrix))
