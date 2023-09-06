dcaedb7564b2a7564ee7e0222eb575b3dec47cdb
Ote Ian <oteian9@gmail.com>
2023-09-07 01:01:07 +0300

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,6 +13,9 @@ def add_integer(a, b=98):
     Return:
         int: Sum of a and b
     """
+    if a == float('inf') or b == float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
+
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     elif type(b) is not int and type(b) is not float:
