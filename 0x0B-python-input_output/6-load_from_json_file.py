bf137a8b1aa64b3e309fc0ebf1975544d167e5b8
Ote Ian <oteian9@gmail.com>
2023-09-12 12:33:26 -0700

    .

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""  """
+"""
+This script defines a function 'load_from_json_file' that loads a Python object
+from a JSON file
+
+Usage:
+    To use this script, call the 'load_from_json_file' function with a
+    filename as an argument
+
+Example:
+    loaded_data = load_from_json_file("input.json")
+"""
 
 
 import json
@@ -8,4 +18,4 @@ import json
 def load_from_json_file(filename):
     """  """
     with open(filename, 'r') as file:
-        json.load(filename, file)
+        json.load(filename)
