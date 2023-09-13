35b6d978d28bad2d07ebdf979f14de97a2f1126d
Ote Ian <oteian9@gmail.com>
2023-09-13 20:39:08 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This script defines a function 'class_to_json' that converts the attributes of a class object to a JSON-compatible dictionary.
+
+Usage:
+    To use this script, call the 'class_to_json' function with a class object as an argument.
+
+Example:
+    class MyClass:
+        def __init__(self):
+            self.name = "John"
+            self.age = 30
+
+    my_instance = MyClass()
+    json_data = class_to_json(my_instance)
+"""
+
+
+def class_to_json(obj):
+    """
+    Convert the attributes of a class object to a JSON-compatible dictionary.
+
+    Parameters:
+        obj: The class object to be converted to a dictionary.
+
+    Returns:
+        dict: A dictionary containing the attributes of the class object.
+
+    Note:
+        Only attributes of type list, dict, str, int, and bool are included in the dictionary.
+    """
+    data = {}
+
+    for name in dir(obj):
+        if not name.startswith("__"):
+            value = getattr(obj, name)
+            if isinstance(value, (list, dict, str, int and bool)):
+                data[name] = value
+            
+    return data
