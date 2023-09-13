18e8ebc438dc18d897611aef460e1f13cd1e378d
Ote Ian <oteian9@gmail.com>
2023-09-13 21:15:15 +0300

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This script defines a class 'Student' with methods for initializing and
+converting a student object to a JSON-compatible dictionary.
+
+Usage:
+    To use this script, create an instance of the 'Student' class and use the
+    'to_json' method to convert it to a dictionary.
+
+Example:
+    student = Student("John", "Doe", 20)
+    student_data = student.to_json()
+"""
+
+
+class Student:
+    """
+    A class representing a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Convert the Student object to a JSON-compatible dictionary.
+
+        Returns:
+            dict: A dictionary containing the attributes of the Student object
+        """
+        data = {}
+
+        for name in dir(self):
+            value = getattr(self, name)
+            if isinstance(value, (list, dict, str, int, bool)):
+                data[name] = value
+
+        return data
