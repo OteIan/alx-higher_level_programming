75618f4b6ceb4d3ff9288eaf17042141b716ac5f
OteIan <oteian9@gmail.com>
2023-11-13 11:34:25 -0800

    Value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-
-const { argv } = require('node:process');
-
-if (length(argv) == 0)
-    console.log('No argument');
-else
-    console.log(argv0);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
