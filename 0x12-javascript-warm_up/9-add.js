180e55526616d9a693cf1e7f78528ff2441353b9
OteIan <oteian9@gmail.com>
2023-11-15 05:12:06 -0800

    Add correction

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function add(a, b) {
-    return a + b;
+const x = parseInt(process.argv[2]);
+const y = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
 }
 
-const a = process.argv[2];
-const b = process.argv[3];
-
-const sum = add(a, b);
+const sum = add(x, y);
 console.log(sum);
