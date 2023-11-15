8a71209769eb0a3d29d652aaa9db7e5e849bde8b
OteIan <oteian9@gmail.com>
2023-11-15 05:43:21 -0800

    Correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,12 @@
 const list = process.argv.slice(2).map(Number);
 
 if (list.length === 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    const max_num = Math.max(...list);
-    const index = list.indexOf(max_num);
-    list.splice(index, 1);
+  const maxNum = Math.max(...list);
+  const index = list.indexOf(maxNum);
+  list.splice(index, 1);
 
-    const second_max_num = Math.max(...list);
-    console.log(second_max_num);
+  const secondMaxNum = Math.max(...list);
+  console.log(secondMaxNum);
 }
