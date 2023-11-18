c5aa8f7de34f0f0ff43e16266035974a1dcc6799
Ote Ian <oteian9@gmail.com>
2023-11-18 13:20:04 +0300

    New correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """
 Script that lists all states from the database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
 
 
 def list_states(username, password, database):
