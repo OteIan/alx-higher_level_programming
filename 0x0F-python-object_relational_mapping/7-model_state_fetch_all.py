eb8f60530fabae9dc4fbf739375d1e00a93bfe06
Ote Ian <oteian9@gmail.com>
2023-11-19 06:26:40 +0000

    Correction: Documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Module that connects to the database and querries it
 """
 import sys
 from model_state import State, Base
@@ -10,17 +11,27 @@ from sqlalchemy.orm import sessionmaker
 def list_state_objects(user, password, db):
     """
     Lists all state objects from the database hbtn_0e_6_usa
+
+    Args:
+    - user (str): MySQL username
+    - password (str): MySQL password
+    - db (str): MySQL database name
     """
+    # Constructing the database URL
     url = f"mysql+mysqldb://{user}:{password}@localhost:3306/{db}"
 
+    # Creating an SQLAlchemy engine
     engine = create_engine(url, pool_pre_ping=True)
 
+    # Creating the database tables
     Base.metadata.create_all(engine)
 
+    # Creating an SQLAlchemy session
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id)
 
+    # Querying all state objects and printing their information
+    states = session.query(State).order_by(State.id)
     for state in states:
         print(state.id, state.name, sep=": ")
 
