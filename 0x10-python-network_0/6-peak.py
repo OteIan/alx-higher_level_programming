7932c8dd63919df2ff8e1925a76a87f16c9290c5
Ote Ian <oteian9@gmail.com>
2023-12-02 18:55:32 +0300

    Efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,25 @@ def find_peak(list_of_integers):
     """ Finds the peak in a list of unsorted integers"""
     if list_of_integers == []:
         return None
-    return max(list_of_integers)
+    
+    length = len(list_of_integers)
+    i = int(length / 2)
+    li = list_of_integers
+    
+    if i - 1 < 0 and i + 1 >= length:
+        return li[i]
+    elif i - 1 < 0:
+        if li[i] > li[i + 1]:
+            return li[i]
+        return li[i + 1]
+    elif i + 1 >= length:
+        if li[i] > li[i - 1]:
+            return li[i]
+        return li[i - 1]
+    
+    if li[i - 1] < li[i] > li[i + 1]:
+        return li[i]
+    
+    if li[i + 1] > li[i - 1]:
+        return find_peak(li[i:])
+    return find_peak(li[:i])
