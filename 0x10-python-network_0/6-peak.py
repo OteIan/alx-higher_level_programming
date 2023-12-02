7491cec24368cfe99c30c880efc796ac5abc3d97
Ote Ian <oteian9@gmail.com>
2023-12-02 15:46:19 +0000

    Peak - First attempt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,6 @@
 
 def find_peak(list_of_integers):
     """ Finds the peak in a list of unsorted integers"""
-    if list_of_integers is None:
+    if list_of_integers == []:
         return None
-    return list_of_integers.max()
+    return max(list_of_integers)
