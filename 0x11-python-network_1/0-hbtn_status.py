2cef747ebe05c3cd6fcb8e37e228a722876ed704
Ote Ian <oteian9@gmail.com>
2023-12-06 02:33:13 +0000

    Whats my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Fetches a URL """
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    content = body.decode('utf-8')
+
+if __name__ == "__main__":
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf-8 content: {content}")
