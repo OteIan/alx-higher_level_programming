7f1a3e8a46a3675db321c6564d1de3f6d1238a55
Ote Ian <oteian9@gmail.com>
2023-12-27 17:16:09 +0300

    Technical interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    name, owner = sys.argv[1:]
+    url = f"https://api.github.com/repos/{name}/{owner}/commits"
+
+    commits = requests.get(url).json()
+
+    try:
+        for i in range(10):
+            sha = commits[i].get('sha')
+            author_name = commits[i].get('commit').get('author').get('name')
+            print(f"{sha}: {author_name}")
+
+    except IndexError:
+        pass
